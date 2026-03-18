bb85c3b6a4fa021bb1bc7002994f63cc8a6f8368
karandevang <karanhbkeerthi@gmail.com>
2026-03-17 20:45:05 -0400

    fix router basename for github pages

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -5,15 +5,20 @@ import { Projects } from "./pages/Projects";
 import { Contact } from "./pages/Contact";
 import { Layout } from "./components/Layout";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Layout,
+      children: [
+        { index: true, Component: Home },
+        { path: "about", Component: About },
+        { path: "projects", Component: Projects },
+        { path: "contact", Component: Contact },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: Layout,
-    children: [
-      { index: true, Component: Home },
-      { path: "about", Component: About },
-      { path: "projects", Component: Projects },
-      { path: "contact", Component: Contact },
-    ],
-  },
-]);
+    basename: "/my_portfolio/",
+  }
+);
